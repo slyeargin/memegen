41ce16beace832e1eca178e17124aa374f2317eb
Kelley Stephens <kelley.stephens@outlook.com>
2014-06-04 20:24:37 -0500

    share btn added

--- app/static/js/es6/main.es6.js
+++ app/static/js/es6/main.es6.js
@@ -21,7 +21,9 @@
     var name = $('#name').val();
     var tag = $('#memes option:selected').text();
     var url = $('#memes option:selected').data('url');
-    ajax('/memes/create', 'post', {top:top, bottom:bottom, name:name, tag:tag, url:url}, m=>{
+    var width = $('#memes option:selected').data('width');
+    var height = $('#memes option:selected').data('height');
+    ajax('/memes/create', 'post', {top:top, bottom:bottom, name:name, tag:tag, url:url, width:width, height:height}, m=>{
       window.location= `/memes/${m._id}`;
     });
   }
@@ -32,14 +34,14 @@
     var url = $('#memes option:selected').data('url');
     var width = $('#memes option:selected').data('width');
     var height = $('#memes option:selected').data('height');
-    $('#showMeme').html(`<input id='name' placeholder='Name Your Meme' style='width:${width - 6}px' /><div id='pic' style='background-image:url(${url}); width:${width}px; height:${height}px'><input id='top' placeholder='Type top caption here' /><input id='bottom' placeholder='Type bottom caption here' /></div>`);
+    $('#showMeme').html(`<input id='name' placeholder='Name Your Meme' style='width:${width - 6}px' /><div id='pic' style='background-image:url(${url}); width:${width}px; height:${height}px'><input class='top' placeholder='Type top caption here' /><input class='bottom' placeholder='Type bottom caption here' /></div>`);
   }
 
   function getMemes(){
     ajax('/memes', 'get', null, obj=>{
       var meme = obj.data.memes;
       meme.forEach(m=>{
-        $('#memes').append(`<option data-url=${m.url} data-height=${m.height} data-width=${m.width} class='meme'>${m.name}</option>`);
+        $('#memes').append(`<option data-url=${m.url} data-height=${m.height} data-width=${m.width} class='memes'>${m.name}</option>`);
       });
       $('#memes').show();
     });
